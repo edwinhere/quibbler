b4b8bcf6650ee13878175300d521f5f5203dcf67
Edwin Jose P <edwinhere@gmail.com>
2018-12-09 11:22:33 +0800

    Added missing pipe needed for psql

--- db/db-cronjob.sh
+++ db/db-cronjob.sh
@@ -4,4 +4,4 @@ date
 source /root/.pgvm/pgvm_env
 pgvm use 11.1
 cd "$(dirname "$0")"
-PGPASSWORD=password psql -h localhost -p 6543 -U lovegood -d quibbler periodic-db-tasks.sql
+PGPASSWORD=password psql -h localhost -p 6543 -U lovegood -d quibbler < periodic-db-tasks.sql
